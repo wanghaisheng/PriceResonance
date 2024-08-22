bb5ca3c7fd6d767d87ee449ec39af3f6a81214e8
wanghaisheng <edwin_uestc@163.com>
2024-08-22 22:07:17 +0800

    fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -89,10 +89,27 @@ const insightsCollection = defineCollection({
 });
 
 
+const blogsCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) => z.object ({
+  title: z.string(),
+  description: z.string(),
+  // contents: z.array(z.string()),
+  author: z.string(),
+  role: z.string().optional(),
+  authorImage: image(),
+  authorImageAlt: z.string(),
+  pubDate: z.date(),
+  cardImage: image(),
+  cardImageAlt: z.string(),
+  readTime: z.number(),
+  tags: z.array(z.string()).optional(),
+  }),
+});
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
   'products': productsCollection,
   'blog': blogCollection,
-  'blogs': insightsCollection,
+  'blogs': blogsCollection,
   'insights': insightsCollection,
 };
