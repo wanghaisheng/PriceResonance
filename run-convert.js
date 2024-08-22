a59828f76aa411fc7333683f41216030a01fa643
wanghaisheng <edwin_uestc@163.com>
2024-08-23 04:53:49 +0800

    add image process scripts

--- run-convert.js
+++ run-convert.js
@@ -1,7 +1,11 @@
-const path = require('path');
-const { spawn } = require('child_process');
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { spawn } from 'child_process';
 
-const scriptPath = path.join(__dirname, 'scripts', 'convertToAvif.js');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const scriptPath = path.join(__dirname, 'scripts', 'convertToAvif.mjs');
 
 const child = spawn('node', [scriptPath], { stdio: 'inherit' });
 
