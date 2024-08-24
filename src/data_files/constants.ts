b209992e1661ea036e6b0b7154c5cd92c255e189
wanghaisheng <edwin_uestc@163.com>
2024-08-24 09:06:50 +0800

    add blog3

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -2,9 +2,9 @@ import ogImageSrc from "@images/social.png";
 
 export const SITE = {
   title: "PriceResonance",
-  tagline: "Top-quality Hardware Tools",
-  description: "PriceResonance offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short: "PriceResonance offers top-tier hardware tools and expert construction services to meet all your project needs.",
+  tagline: "Top saas price track Tools",
+  description: "Building price dataset with PriceResonance.straightforward with a bunch of url list,natural language task description,AI take care of everything.",
+  description_short: "Building price dataset with PriceResonance.straightforward with a bunch of url list,natural language task description,AI take care of everything.",
   url: "https://priceresonance.com",
   author: "Emil Gulamov",
 };
@@ -33,7 +33,7 @@ export const OG = {
   locale: "en_US",
   type: "website",
   url: SITE.url,
-  title: `${SITE.title}: : Hardware Tools & Construction Services`,
-  description: "Equip your projects with PriceResonance's top-quality hardware tools and expert construction services. Trusted by industry leaders, PriceResonance offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
+  title: `${SITE.title}: : tool that help to Track your competitor pricing strategy with LLM No code No point click method`,
+  description: "Building price dataset with PriceResonance.straightforward with a bunch of url list,natural language task description,AI take care of everything.",
   image: ogImageSrc,
 };
